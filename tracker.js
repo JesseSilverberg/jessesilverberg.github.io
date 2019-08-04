b77ca635839b0407b6be885bade2793c6c6600a8
Jesse Silverberg <jessesilverberg11@gmail.com>
2019-08-04 19:04:08 -0400

    remove asking

--- tracker.js
+++ tracker.js
@@ -37,24 +37,4 @@ function getAddress (latitude, longitude) {
    )
 }
 
-if ("geolocation" in navigator) {
-  // check if geolocation is supported/enabled on current browser
-  navigator.geolocation.getCurrentPosition(
-   function success(position) {
-     // for when getting location is a success
-     console.log('latitude', position.coords.latitude,
-                 'longitude', position.coords.longitude);
-     //getAddress(position.coords.latitude, position.coords.longitude)
-   },
-  function error(error_message) {
-    // for when getting location results in an error
-    console.error('An error has occured while retrieving' +
-                  'location', error_message)
-    ipLookUp()
-  });
-} else {
-  // geolocation is not supported
-  // get your location some other way
-  console.log('geolocation is not enabled on this browser')
-  ipLookUp()
-}
+ipLookUp();
